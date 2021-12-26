be8b53419f1cfb8388edf473c3e3c47edcf8ba2a
Maxisthemoose <oriontothemax@gmail.com>
2021-12-25 21:28:19 -0800

    Started class comments

--- src/Pieces/Piece.js
+++ src/Pieces/Piece.js
@@ -1,17 +1,35 @@
+
+/**
+ * The basic unspecified piece, this is used to build all
+ * the other pieces from.
+ */
 class Piece {
   /**
+   * the current location of the piece in board cords [y, x]
    * @type {[number, number]}
    */
   location;
+  // if the piece has moved
   has_moved = false;
+  /**
+   * the color of the piece
+   * @type {"w" | "b"}
+   */
   color;
+  // only used for pawns really, so they move in the correct dirrection
   moveMulti;
+  // actual piece type (ie: wp = White Pawn, or bk = Black King)
   piece_type;
   /**
+   * The initial position of the piece
    * @type {[number, number]}
    */
   startingLocation;
 
+  /**
+   * @param {[number, number]} location 
+   * @param {"b" | "w"} color 
+   */
   constructor(location, color) {
     this.location = location;
     this.color = color;
@@ -20,23 +38,10 @@ class Piece {
   }
 
   /**
-   * @param {[number, number]} location
-   */
-  set location(location) {
-    this.location = location;
-  }
-
-  /**
-   * @param {string[][]} board 
-   * @param {Piece[]} otherPieces
-   * @param {string} enPassant
+   * @param {string[][]} board the board context in array form
+   * @param {Piece[]} otherPieces the array of other pieces in the game
+   * @param {string} enPassant en passant square if applicable (really only used in pawns)
    * @returns {[number, number][]}
    */
   getMoves(board, otherPieces, enPassant) {};
-  /**
-   * @param {boolean} new_val
-   */
-  set has_moved(new_val) {
-    this.has_moved = new_val;
-  }
 }
